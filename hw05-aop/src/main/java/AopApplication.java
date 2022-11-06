6996edfb339df091af85cb5cc6d23effc5eb8b3b
barmatin <barmatin.andrey@mail.ru>
2022-11-06 16:09:03 +0700

    hw05-aop исправление замечаний

--- hw05-aop/src/main/java/AopApplication.java
+++ hw05-aop/src/main/java/AopApplication.java
@@ -1,6 +1,6 @@
 public class AopApplication {
     public static void main(String[] args) {
-        TestLogging testLogging = Ioc.createTestLoggingImpl();
+        TestLogging testLogging = Ioc.createTestLoggingImpl(new TestLoggingImpl());
         testLogging.calculation(6);
     }
 }
--- hw05-aop/src/main/java/Ioc.java
+++ hw05-aop/src/main/java/Ioc.java
@@ -2,35 +2,45 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
+import java.util.*;
 
 class Ioc {
 
     private Ioc() {
     }
 
-    static TestLogging createTestLoggingImpl() {
-        InvocationHandler handler = new CustomInvocationHandler(new TestLoggingImpl());
+    static TestLogging createTestLoggingImpl(TestLogging testLogging) {
+        InvocationHandler handler = new CustomInvocationHandler(testLogging);
         return (TestLogging) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
                 new Class<?>[]{TestLogging.class}, handler);
     }
 
     static class CustomInvocationHandler implements InvocationHandler {
         private final TestLogging testLogging;
+        private final List<Method> annotatedMethodList;
 
         CustomInvocationHandler(TestLogging testLogging) {
             this.testLogging = testLogging;
+            List<Method> annotatedMethodList = new ArrayList<>();
+            for (Method method: testLogging.getClass().getDeclaredMethods()) {
+                for (Annotation annotation: method.getDeclaredAnnotations()) {
+                    if (annotation.annotationType() == Log.class) {
+                        annotatedMethodList.add(method);
+                    }
+                }
+            }
+            this.annotatedMethodList = annotatedMethodList;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Annotation annotation = testLogging.getClass()
-                    .getMethod(method.getName(), method.getParameterTypes())
-                    .getAnnotation(Log.class);
-            if (annotation != null) {
-                System.out.println("executed method: " + method.getName() + ", params: " + Arrays.toString(args));
+            for (Method annotatedMethod: annotatedMethodList) {
+                if (annotatedMethod.getName().equals(method.getName()) &&
+                        Arrays.toString(annotatedMethod.getParameterTypes()).equals(Arrays.toString(method.getParameterTypes()))) {
+                    System.out.println("executed method: " + method.getName() + ", params: " + Arrays.toString(args));
+                    break;
+                }
             }
-
             return method.invoke(testLogging, args);
         }
 
